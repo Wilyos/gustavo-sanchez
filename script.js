65a82f6cfa5cf9fe01fccf6868af6d27fd23aabf
wilyos <wilyos99@gmail.com>
2025-06-05 12:36:04 -0500

    actualizacion

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let contacto = document.querySelector('#btn-contacto');
+let sobreMi = document.querySelector('#btn-sobreMi')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+contacto.onclick = (e) => {
+    e.preventDefault();
+    const downloadLink = document.querySelector('#downloadContact');
+    if (downloadLink) {
+        downloadLink.click();
+    }
+};
+
+
+sobreMi.onclick = () => {
+    document.getElementById("sobreMi").scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+const vcardData = `
+BEGIN:VCARD
+VERSION:3.0
+FN:Gustavo Sanchez
+ORG:Consultores Asociados
+TEL;TYPE=CELL:+57 3137685544
+EMAIL:ca@consultoresasociados.com.co
+URL:https://www.instagram.com/consultoresmed/
+END:VCARD
+  `.trim();
+
+
+
+  const blob = new Blob([vcardData], { type: 'text/vcard' });
+  const url = URL.createObjectURL(blob);
+
+  const downloadLink = document.getElementById('downloadContact');
+  downloadLink.href = url;
